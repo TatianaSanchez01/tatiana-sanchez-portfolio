2d642cfe01b5aa88fa08fbdb101346fb8fd8f11f
TatianaSanchez01 <107728796+TatianaSanchez01@users.noreply.github.com>
2024-01-05 17:36:28 -0500

    feat: :sparkles: added footer component

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Link from "next/link";
+import Layout from "./Layout";
+const Footer = () => {
+    return (
+        <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg ">
+            <Layout className="py-10 flex items-center justify-between ">
+                <span>
+                    {new Date().getFullYear()} &copy; All Rights Reserved.
+                </span>
+                <div className="flex items-center">
+                    Build with{" "}
+                    <span className="text-primary text-2xl px-1">&#9825;</span>
+                    by&nbsp;{" "}
+                    <Link
+                        href="https://www.linkedin.com/in/tatiana-sanchez-sanin/"
+                        className="underline underline-offset-2"
+                        target="_blank"
+                    >
+                        Tatiana Sanchez
+                    </Link>
+                </div>
+                <Link href="/contact" className="underline underline-offset-2">
+                    Say Hello
+                </Link>
+            </Layout>
+        </footer>
+    );
+};
+
+export default Footer;
